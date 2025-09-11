9032dbfe50bb53b365fb2ad028f44c0da1718e4e
Boualem234 <2534056@etu.cchic.ca>
2025-09-11 10:43:16 -0400

    ok

--- Website/js/script.js
+++ Website/js/script.js
@@ -6,27 +6,27 @@
 
 //mettre le radius dans l'api
 async function getTilesAsync(x, y) {
-    try {
-        const url = `https://localhost:7039/api/Tuiles/${x}/${y}`;
-        const response = await fetch(url);
-      
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP: ${response.status}`);
+  try {
+      const url = `https://localhost:7039/api/Tuiles/${x}/${y}`;
+      const response = await fetch(url);
+    
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
+    }
+    
+    const tiles = await response.json();
+    displayTiles(tiles);
+    
+  } catch (error) {
+    console.error('Erreur lors du chargement:', error);
+    showErrorMessage('Impossible de charger les tuiles');
+  }
+}
+
+async function getBaseTitles(){
+  for (let x = 9; x <= 11; x++) {
+      for (let y = 9; y <= 11; y++) {
+          await getTilesAsync(x, y);
       }
-      
-      const tiles = await response.json();
-      displayTiles(tiles);
-      
-    } catch (error) {
-      console.error('Erreur lors du chargement:', error);
-      showErrorMessage('Impossible de charger les tuiles');
-    }
   }
-  
-  async function getBaseTitles(){
-    for (let x = 9; x <= 11; x++) {
-        for (let y = 9; y <= 11; y++) {
-            await getTilesAsync(x, y);
-        }
-    }
-  }
+}
