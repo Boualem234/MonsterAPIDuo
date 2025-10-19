eb0bb280da4f528698cd50b1c2d0b459e5dd26e3
Bubeos <2531003@etu.cchic.ca>
2025-10-19 18:09:52 -0400

    corrigé

--- MonsterAPI-main/MyLittleRPG/Controllers/CharactersController.cs
+++ MonsterAPI-main/MyLittleRPG/Controllers/CharactersController.cs
@@ -175,63 +175,6 @@ namespace MyLittleRPG_ElGuendouz.Controllers
             }
         }
 
-        // GET: api/Characters
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Character>>> GetCharacter()
-        {
-            return await _context.Character.ToListAsync();
-        }
-
-        // GET: api/Characters/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Character>> GetCharacter(int id)
-        {
-            var character = await _context.Character.FindAsync(id);
-            return character == null ? NotFound() : Ok(character);
-        }
-
-        // PUT: api/Characters/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCharacter(int id, Character character)
-        {
-            if (id != character.idPersonnage) return BadRequest();
-
-            _context.Entry(character).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CharacterExists(id)) return NotFound();
-                throw;
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Characters
-        [HttpPost]
-        public async Task<ActionResult<Character>> PostCharacter(Character character)
-        {
-            _context.Character.Add(character);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction("GetCharacter", new { id = character.idPersonnage }, character);
-        }
-
-        // DELETE: api/Characters/5
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCharacter(int id)
-        {
-            var character = await _context.Character.FindAsync(id);
-            if (character == null) return NotFound();
-
-            _context.Character.Remove(character);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
 
         [HttpGet("Simulate/{monstreId}/{email}")]
         public ActionResult<CombatResultDto> Simulate(int monstreId, string email)
